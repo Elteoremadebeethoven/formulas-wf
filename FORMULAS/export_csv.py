a696fb260c1ec756143db0f99a4e8969c404ea52
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2020-07-16 08:10:58 -0500

    Update export_csv.py

--- FORMULAS/export_csv.py
+++ FORMULAS/export_csv.py
@@ -1,9 +1,10 @@
 import argparse
 import posixpath
 import csv
-from io import *
 import os
+import re
 import numpy as np
+from io import *
 
 ALL_STRINGS = []
 def clean_screen():
@@ -40,22 +41,35 @@ ALL_STRINGS = [
 ]
 
 INDEX = []
-count = 0;
+REAL_INDEX = []
+count = 0
+real_count = 0
 while count < len(formula_selected):
     s = formula_selected[count]
     if s not in [" ","\n"]:
         clean_screen()
         print(ALL_STRINGS[0])
         spaces = " "*count
-        for pre_write in INDEX:
-            print(f"\t\t{pre_write[0]}\t|| {pre_write[1]}")
+
+        for pre_write in REAL_INDEX:
+            if pre_write[1] != None:
+                print(f"\t\t{pre_write[1][0]}\t|| {pre_write[1][1]}")
         print(f"    {spaces}|")
         print(f"    {spaces}v")
         print(ALL_STRINGS[1])
         input_string = input(f"\t\t{count+1})\t{s} || ")
+        if input_string[:len("BACK")] == "BACK":
+            if input_string == "BACK":
+                back_lines = 1
+            else:
+                back_str, back_lines = input_string.split()
+            count -= int(back_lines)
+            del REAL_INDEX[-int(back_lines):]
+            continue
         if input_string.isnumeric():
             pair_append = (s,input_string)
             INDEX.append(pair_append)
+            REAL_INDEX.append([count, pair_append])
         elif s == "\\":
             tex_symbol, *tex_indexes = input_string.split()
             more_spaces = len(tex_symbol)
@@ -64,12 +78,19 @@ while count < len(formula_selected):
                 if len(tex_indexes) == 1:
                     pair_append = (tex_symbol,ti)
                     INDEX.append(pair_append)
+                    REAL_INDEX.append([count, pair_append])
                 else:
                     pair_append = (f"{tex_symbol}_{sub_count}",ti)
                     INDEX.append(pair_append)
+                    REAL_INDEX.append([count, pair_append])
                 sub_count += 1
             count += more_spaces
+        else:
+            REAL_INDEX.append([count, None])
+    else:
+        REAL_INDEX.append([count, None])
     count += 1
+
 
 print("The indexes are:")
 for s,i in INDEX:

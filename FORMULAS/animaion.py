cc5101ad02a9c7cdae125b033f6654bd95acb3c7
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2020-09-25 13:19:26 -0500

    Update animaion.py

--- FORMULAS/animaion.py
+++ FORMULAS/animaion.py
@@ -36,60 +36,71 @@ class SolveGeneralQuadraticEquation(Scene):
         ]
 
     def step_formula(self,
-                            pre_write=[],
                             pos_write=[],
                             pre_fade=[],
-                            pos_fade=[],
                             fade=[],
                             write=[],
-                            changes=[[]],
-                            path_arc=0,
-                            n_step=0,
                             pre_copy=[],
                             pos_copy=[],
                             time_pre_changes=0.3,
                             time_pos_changes=0.3,
                             run_time=2,
                             time_end=0.3,
-                            pre_order=["w","f"],
-                            pos_order=["w","f"]
                             ):
         print(f"step: {self.count}")
         n_step = self.count
         formula_copy=[]
-        for c in pre_copy:
-            formula_copy.append(self.formulas[n_step-1][c].copy())
 
-        for ani_ in pre_order:
-            if len(pre_write)>0 and ani_=="w":
-                self.play(*[Write(self.formulas[n_step-1][w])for w in pre_write])
-            if len(pre_fade)>0 and ani_=="f":
-                self.play(*[FadeOut(self.formulas[n_step-1][w])for w in pre_fade])
+        pre_formula = self.formulas[n_step - 1]
+        pos_formula = self.formulas[n_step]
+
+        for c in pre_copy:
+            formula_copy.append(pre_formula[c].copy())
+
+        if len(pre_fade) > 0:
+            self.play(*[
+                FadeOut(pre_formula[w])
+                for w in pre_fade
+            ])
 
         self.wait(time_pre_changes)
 
         for pre_ind,post_ind in self.set_of_changes[n_step-1]:
-            self.play(*[
-                ReplacementTransform(
-                    self.formulas[n_step-1][i],self.formulas[n_step][j],
-                    path_arc=path_arc
+            self.play(
+                *[
+                    ReplacementTransform(
+                        pre_formula[i],pos_formula[j],
                     )
-                for i,j in zip(pre_ind,post_ind)
+                    for i,j in zip(pre_ind,post_ind)
                 ],
-                *[FadeOut(self.formulas[n_step-1][f])for f in fade if len(fade)>0],
-                *[Write(self.formulas[n_step][w])for w in write if len(write)>0],
-                *[ReplacementTransform(formula_copy[j],self.formulas[n_step][f])
-                for j,f in zip(range(len(pos_copy)),pos_copy) if len(pre_copy)>0
+                *[
+                    FadeOut(pre_formula[f])
+                    for f in fade
+                ],
+                *[
+                    Write(pos_formula[w])
+                    for w in write
+                ],
+                *[
+                    ReplacementTransform(
+                        formula_copy[j],
+                        pos_formula[f]
+                    )
+                    for j,f in zip(
+                                    range(len(pos_copy)),
+                                    pos_copy
+                                )
                 ],
                 run_time=run_time
             )
 
         self.wait(time_pos_changes)
+        
+        if len(pos_write) > 0:
+            self.play(*[
+                Write(pos_formula[w])
+                for w in pos_write
+            ])
 
-        for ani_ in pos_order:
-            if len(pos_write)>0 and ani_=="w":
-                self.play(*[Write(self.formulas[n_step][w])for w in pos_write])
-            if len(pos_fade)>0 and ani_=="f":
-                self.play(*[FadeOut(self.formulas[n_step][w])for w in pos_fade])
         self.wait(time_end)
         self.count += 1
